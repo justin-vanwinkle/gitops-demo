076a4039292d02c835ecf6267ad787e23bfdc367
Justin VanWinkle <vanwinkle.justin@gmail.com>
2022-12-08 11:33:00 -0500

    Update virtual_machines.tf

--- virtual_machines.tf
+++ virtual_machines.tf
@@ -1,12 +1,12 @@
 
-module "ops_vm" {
-  source = "./virtual-machine"  
+# module "ops_vm" {
+#   source = "./virtual-machine"  
   
-  vm_name           = "ops-vm-001"
-}
+#   vm_name           = "ops-vm-001"
+# }
 
-module "eweb_debug_vm" {
-  source = "./virtual-machine"  
+# module "eweb_debug_vm" {
+#   source = "./virtual-machine"  
   
-  vm_name           = "eweb-vm-001"
-}
+#   vm_name           = "eweb-vm-001"
+# }
