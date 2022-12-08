dc786cfe3035b1db23f672f5f490a6635f3cfa86
Justin VanWinkle <vanwinkle.justin@gmail.com>
2022-12-08 13:13:34 -0500

    Update resource_groups.tf

--- resource_groups.tf
+++ resource_groups.tf
@@ -1,4 +1,4 @@
-# resource "azurerm_resource_group" "demo_rg_1" {
-#   name     = "demo-rg-1"
-#   location = "East US"
-# }
+resource "azurerm_resource_group" "demo_rg_1" {
+  name     = "demo-rg-1"
+  location = "East US"
+}
